d3e19ae4088a5a71b82e4bcd5a1dd6bfd7fbaf0d
andrew4699 <andrew.guterman1@gmail.com>
2024-09-11 21:52:19 -0700

    Some work to inject TestEnvironment

--- polaris-service/build.gradle.kts
+++ polaris-service/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
   testImplementation("org.apache.iceberg:iceberg-api:${libs.versions.iceberg.get()}:tests")
   testImplementation("org.apache.iceberg:iceberg-core:${libs.versions.iceberg.get()}:tests")
+  testImplementation("org.apache.iceberg:iceberg-common")
   testImplementation("io.dropwizard:dropwizard-testing")
   testImplementation(platform(libs.testcontainers.bom))
   testImplementation("org.testcontainers:testcontainers")
@@ -233,6 +234,7 @@ tasks.named<Jar>("jar") {
 }
 
 tasks.register<Jar>("testJar") {
+  // TODO: make "assemble" run this
   archiveClassifier.set("tests")
   from(sourceSets.test.get().output)
 }
